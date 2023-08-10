a693ce013f3e64becf6c207c5b419ad48b1cc301
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-08-10 12:03:13 +0300

    update profile seeder

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -20,15 +20,15 @@ class ProfileSeeder extends Seeder
             'email'               => 'pmizengo@gmail.com',
             'phone'               => '+255 786 397 123',
             'location'            => 'Dar es Salaam, Tanzania',
-            'designation'         => 'Full Stack Web Developer',
+            'designation'         => 'Full Stack Developer',
             'profile_image'       => 'profile_low.png',
-            'profile_description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore possimus cum esse aut alias sint at ut earum, quod quam, tempora sed fuga illum? A! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore possimus cum esse aut alias sint at ut earum, quod quam, tempora sed fuga illum? A!',
+            'profile_description' => 'My objective is to make use of knowledge, talent and experience of business information communication Technology field to serve the community so as to bring about sustainable development as well as to build career in a growing organization, where i can get opportunity to prove my abilities by accepting challenges, fulfilling the organizational goal and climb the career ladder through continuous learning and commitment.',
             'cv_link'             => 'https://',
             'hire_link'           => 'https://',
             'facebook_link'       => 'https://',
-            'github_link'         => 'https://',
-            'linkedin_link'       => 'https://',
-            'twitter_link'        => 'https://',
+            'github_link'         => 'https://github.com/ngoijr3107',
+            'linkedin_link'       => 'https://www.linkedin.com/in/paschal-mizengo-3b19a585/',
+            'twitter_link'        => 'https://twitter.com/paschalngoi',
             'created_at'          => Carbon::now()
         ]);
     }
